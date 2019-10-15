e853607d3facaa30fed2f2f3a163f3fdadd4c0e4
marszm <mariusz.szmer@gmail.com>
2019-10-15 21:07:19 +0200

    add kafka consumer

--- database-service/src/main/java/com/example/databaseservice/restcontroller/HelloWorldController.java
+++ database-service/src/main/java/com/example/databaseservice/restcontroller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.example.databaseservice.restcontroller;
 import com.example.databaseservice.model.HelloWorld;
 import com.example.databaseservice.repository.HelloWorldRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,25 @@ import java.util.List;
 @RestController
 public class HelloWorldController {
 
+//    List<String> messages = new ArrayList<>();
+    String message;
+
     @Autowired
     HelloWorldRepository helloWorldRepository;
+
+    @KafkaListener(groupId = "topic1config", topics = "topic1", containerFactory = "kafkaListenerContainerFactory")
+//    @SendTo
+    public String getMsg(String data) {
+        message = data;
+        return data;
+    }
+
+    @GetMapping("/consumer")
+    public String list1() {
+
+        return message;
+
+    }
 
     @GetMapping("/allMessages")
     public List<HelloWorld> list() {
